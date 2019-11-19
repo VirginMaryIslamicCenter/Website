0fecc6738af33767261a3691e934fd2203bfed0c
Farshad Hemmati <farshad@hemmati.com>
2019-11-18 16:44:03 -0800

    add www redirection

--- VirginMaryCenter/Startup.cs
+++ VirginMaryCenter/Startup.cs
@@ -13,84 +13,90 @@ using VirginMaryCenter.Models;
 using VirginMaryCenter.Services;
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace VirginMaryCenter
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(x => Configuration.GetSection("AppConfig").Get<AppConfig>());
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton(x => Configuration.GetSection("AppConfig").Get<AppConfig>());
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
 
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
+			// Add application services.
+			services.AddTransient<IEmailSender, EmailSender>();
 
-            services.AddMvc()
-                .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix, x => x.ResourcesPath = "Resources")
-                .AddDataAnnotationsLocalization();
-        }
+			services.AddMvc()
+				.AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix, x => x.ResourcesPath = "Resources")
+				.AddDataAnnotationsLocalization();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            var supportedCultures = new[]
-            {
-                new CultureInfo("en"),
-                new CultureInfo("pt"),
-                new CultureInfo("es")
-            };
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture(Configuration.GetSection("AppConfig").Get<AppConfig>().About.DefaultCulture),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            });
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			var supportedCultures = new[]
+			{
+				new CultureInfo("en"),
+				new CultureInfo("pt"),
+				new CultureInfo("es")
+			};
+			app.UseRequestLocalization(new RequestLocalizationOptions
+			{
+				DefaultRequestCulture = new RequestCulture(Configuration.GetSection("AppConfig").Get<AppConfig>().About.DefaultCulture),
+				SupportedCultures = supportedCultures,
+				SupportedUICultures = supportedCultures
+			});
 
 
-            if (env.IsDevelopment())
-            {
-                //app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseDeveloperExceptionPage();
+			if (env.IsDevelopment())
+			{
+				//app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseDeveloperExceptionPage();
 
-                //app.UseExceptionHandler("/Home/Error");
-            }
+				//app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
+			app.UseRewriter(new RewriteOptions().AddRedirectToWwwPermanent());
 
-            app.UseStaticFiles();
-            
-            app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "areaRoute",
-                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+			app.UseStaticFiles();
 
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseAuthentication();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "areaRoute",
+					template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+
+
+
 }
